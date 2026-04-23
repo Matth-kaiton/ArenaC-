609ad6a2b0bdf6e5bb415450963db968ba75a06a
Matth-kaiton <matthias.dumas.pro@gmail.com>
2026-04-23 10:54:31 +0200

    supprésion de fichier bin et obj

---
+++ Components/HeroCard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeroArena.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour HeroCard.xaml
+    /// </summary>
+    public partial class HeroCard : UserControl
+    {
+
+        public static readonly DependencyProperty HeroProperty =
+            DependencyProperty.Register(
+                "Hero",
+                typeof(Models.Hero),
+                typeof(HeroCard),
+                new PropertyMetadata(null)
+                );
+
+
+        public Models.Hero Hero
+        {
+            get { return (Models.Hero)GetValue(HeroProperty);}
+            set { SetValue(HeroProperty, value); }
+        }
+
+        public HeroCard()
+        {
+            InitializeComponent();
+        }
+    }
+}

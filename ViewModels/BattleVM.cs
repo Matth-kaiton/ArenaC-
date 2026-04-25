10d3e15552f30068432f54856217b127623e8567
Matth-kaiton <matthias.dumas.pro@gmail.com>
2026-04-26 01:33:05 +0200

    atta battle page visual +  enemi turn to do

---
+++ ViewModels/BattleVM.cs
@@ -0,0 +1,92 @@
+﻿using HeroArena.Commands;
+using HeroArena.Models;
+using HeroArena.ViewsModels;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace HeroArena.ViewModels
+{
+    class BattleVM : BaseVM
+    {
+        private Hero _playerHero;
+        private Hero _enemyHero;
+        private int _currentEnemyHealth;
+        private int _score;
+        private bool _canRestart;
+
+        public Hero PlayerHero { get => _playerHero; set => SetProperty(ref _playerHero, value); }
+        public Hero EnemyHero { get => _enemyHero; set => SetProperty(ref _enemyHero, value); }
+
+        public int CurrentEnemyHealth { get => _currentEnemyHealth; set => SetProperty(ref _currentEnemyHealth, value); }
+
+        public int Score { get => _score; set => SetProperty(ref _score, value); }
+        public bool CanRestart { get => _canRestart; set => SetProperty(ref _canRestart, value); }
+
+        public ICommand AttackCommand { get; }
+        public ICommand RestartCommand { get; }
+
+        public BattleVM()
+        {
+            PlayerHero = MainVM.CurrentHero;
+            Score = 0;
+            GenerateEnemy(true);
+
+            AttackCommand = new RelayCommand(ExecuteAttack);
+            RestartCommand = new RelayCommand(ExecuteRestart);
+        }
+
+        private void ExecuteAttack(object parameter)
+        {
+            var spell = parameter as Spell;
+            if (spell == null || EnemyHero == null || CurrentEnemyHealth <= 0) return;
+
+            CurrentEnemyHealth -= spell.Damage;
+
+            if (CurrentEnemyHealth <= 0)
+            {
+                CurrentEnemyHealth = 0;
+                Score++; 
+                CanRestart = true; 
+            }
+        }
+
+        private void ExecuteRestart(object parameter)
+        {
+            GenerateEnemy(false);
+        }
+
+        private void GenerateEnemy(bool isFirst)
+        {
+            if (isFirst)
+            {
+
+                EnemyHero = new Hero
+                {
+                    Name = "Gbelin",
+                    Health = 100,
+                    Spells = PlayerHero.Spells 
+                };
+            }
+            else
+            {
+                var newHealth = (int)(EnemyHero.Health * 1.10);
+                var improvedSpells = EnemyHero.Spells.Select(s => new Spell
+                {
+                    Name = s.Name,
+                    Damage = (int)(s.Damage * 1.05)
+                }).ToList();
+
+                EnemyHero = new Hero
+                {
+                    Name = $"Ennemi Rang {Score + 1}",
+                    Health = newHealth,
+                    Spells = improvedSpells
+                };
+            }
+
+            CurrentEnemyHealth = EnemyHero.Health;
+            CanRestart = false;
+        }
+    }
+}
--- Views/BattlePage.xaml.cs
+++ Views/BattlePage.xaml.cs
@@ -1,68 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using HeroArena.Models;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using HeroArena.ViewModels;
+
 
 namespace HeroArena.Views
 {
-    public partial class BattlePage : Page
+    public partial class BattlePage : BasePage
     {
-
-        public Models.Hero? FirstHero { get; set; }
         public BattlePage()
         {
             InitializeComponent();
-            CreateHero();
-
-            FirstHero = GetFisrtHero();
-            this.DataContext = this;
         }
 
-        private void CreateHero()
+        override protected object CreateViewModel()
         {
-            using (var db = new Models.ExerciceHeroContext())
-            {
-                if (!db.Heroes.Any())
-                {
-                    db.Heroes.AddRange(
-                        new Hero { Name = "Aragorn", Health = 150, ImageUrl = "https://example.com/aragorn.png" },
-                        new Hero { Name = "Legolas", Health = 100, ImageUrl = "https://example.com/legolas.png" },
-                        new Hero { Name = "Gimli", Health = 180, ImageUrl = "https://example.com/gimli.png" },
-                        new Hero { Name = "Gandalf", Health = 120, ImageUrl = "https://example.com/gandalf.png" }
-                    );
-
-                    db.SaveChanges();
-                }
-            }
-        }
-
-        private void CreateSpells()
-        {
-            using var db = new Models.ExerciceHeroContext();
-            if (!db.Spells.Any())
-            {
-
-            }
-        }
-        private Hero? GetFisrtHero()
-        {
-            using (var db = new Models.ExerciceHeroContext())
-                if (db.Heroes.Any())
-                {
-                    var hero = db.Heroes.FirstOrDefault();
-                    return hero;
-                }
-            return null;
-            // var heros = db.Heros.AsNoTracking().ToList(); pas trop comprit le asnotracking mais utile dans certain cas ce documenter
+            return new BattleVM();
         }
     }
 }
